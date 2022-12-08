7a5f2d524e9794aa8c73b77c0f58fedef2a72c4a
unknown <miamby111@gmail.com>
2022-12-08 11:11:52 +0200

    comparison-operators

--- script/section-11-123-if-else.js
+++ script/section-11-123-if-else.js
@@ -59,26 +59,79 @@ if (value > 0) {
 */
 
 let result;
-if (a + b < 4) { result = "Мало";
+if (a + b < 4) {
+  result = "Мало";
 } else {
   result = "Много";
 }
 //SAME AS ABOVE:
 result = a + b < 4 ? "Мало" : "Много";
 
-
 let message;
-if (login == 'Сотрудник') {
-message = 'Привет';
-} else if (login == 'Директор') {
-message = 'Здравствуйте';
-} else if (login == '') {
-message = 'Нет логина';
+if (login == "Сотрудник") {
+  message = "Привет";
+} else if (login == "Директор") {
+  message = "Здравствуйте";
+} else if (login == "") {
+  message = "Нет логина";
 } else {
-message = '';
+  message = "";
 }
 
 //SAME AS ABOVE:
-let message = (login == 'Сотрудник') ? 'Привет' :
- (login == 'Директор') ? 'Здравствуйте' :
-  (login == '' ) ? 'Нет логина' : '';
+let message =
+  login == "Сотрудник"
+    ? "Привет"
+    : login == "Директор"
+    ? "Здравствуйте"
+    : login == ""
+    ? "Нет логина"
+    : "";
+
+//
+
+let x = 1;
+x > 0 && alert("Greater than zero!");
+
+//SAME AS ABOVE:
+let x = 1;
+if (x > 0) {
+  alert("Greater than zero!");
+}
+
+//
+
+alert(!true); // false
+alert(!0); // true
+
+alert(!!"non-empty string"); // true
+alert(!!null); // false
+//SAME AS ABOVE:
+alert(Boolean("non-empty string")); // true
+alert(Boolean(null)); // false
+
+//
+
+if (age >= 14 && age <= 90);
+//
+if (!(age >= 14 && age <= 90));
+//SAME AS ABOVE:
+if (age < 14 && age > 90);
+
+//
+
+let userName = prompt("who are you?", "");
+if (userName == "admin") {
+  let password = prompt("enter your password", "");
+  if (password == "boss") {
+    alert("Hi boss");
+  } else if (password == "" || password == null) {
+    alert("cancellation");
+  } else {
+    ("Wrong password");
+  }
+} else if (userName == "" || userName == null) {
+  alert("cancellation");
+} else {
+  alert("i dont know you");
+}
---
+++ script/while and for loops.js
@@ -0,0 +1,19 @@
+console.log("while and for loops");
+let i = 0;
+while (i < 3) {
+  alert(i);
+  i++;
+} // outputs: 0 then 1 then 2
+
+//
+let i = 3;
+while (i) { 
+alert( i );
+i--;
+}
+//SAME AS ABOVE:
+let i = 3;
+while (i) alert(i--);
+
+
+// 
